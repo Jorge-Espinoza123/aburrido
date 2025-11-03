b834ccc6fa1c8eda3edb58e1568cd8b5295ef423
Jorge-Espinoza123 <jorgedavidespinozasandoval@gmail.com>
2025-11-03 09:49:45 -0500

    Update drawing.js

--- drawing.js
+++ drawing.js
@@ -1,25 +1,19 @@
 let canvas;
 
-window.addEventListener('load', () => {
-  canvas = new fabric.Canvas('bigcanvas', {
-    isDrawingMode: true
-  });
+window.addEventListener("load", () => {
+  canvas = new fabric.Canvas("bigcanvas");
+  canvas.isDrawingMode = true;
 
-  // Configurar pincel
-  canvas.freeDrawingBrush = new fabric.PencilBrush(canvas);
   canvas.freeDrawingBrush.width = 15;
-  canvas.freeDrawingBrush.color = 'black';
+  canvas.freeDrawingBrush.color = "black";
 
-  // Asegurar fondo blanco
-  canvas.backgroundColor = 'white';
-  canvas.renderAll();
+  canvas.setBackgroundColor("white", canvas.renderAll.bind(canvas));
 
   console.log("🖊️ Listo para dibujar!");
 });
 
 function limpiar() {
   canvas.clear();
-  canvas.backgroundColor = 'white';
-  canvas.renderAll();
+  canvas.setBackgroundColor("white", canvas.renderAll.bind(canvas));
 }
 
